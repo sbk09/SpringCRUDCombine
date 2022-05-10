639f04428a49e9fe5a07723075e96d9023f999cf
AkshayPhase22 <akshayphase11@gmail.com>
2022-05-10 13:29:08 +0530

    second commit

--- src/main/java/com/example/controller/StudentController.java
+++ src/main/java/com/example/controller/StudentController.java
@@ -1,15 +1,19 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.example.entity.Student;
+import com.example.service.SudentService;
 
 public class StudentController {
 	//controller//
+	@Autowired
+	private SudentService service;
 	
 	@PatchMapping("/student/{id}/{marks}")
 	public Student updateMarks(@PathVariable("id") int id, @PathVariable("marks") int marks) {
-		return null;
+		return service.updateMarks(id,marks);
 	}
 }
--- src/main/java/com/example/service/SudentService.java
+++ src/main/java/com/example/service/SudentService.java
@@ -1,5 +1,17 @@
 package com.example.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.entity.Student;
+import com.example.repository.StudentRepository;
+
 public class SudentService {
+     @Autowired
+	private StudentRepository repository;
+	public Student updateMarks(int id, int marks) {
+		Student exitStudent=repository.findById(id).orElse(null);
+		exitStudent.setStd_marks(marks);
+		return repository.save(exitStudent);
+	}
 
 }
