cc72398525f3e6e0912fe51b5b2cc206ba14add1
AkshayPhase22 <akshayphase11@gmail.com>
2022-05-10 13:05:13 +0530

    Commit akshay - added updatemarks method in controller

--- src/main/java/com/example/controller/StudentController.java
+++ src/main/java/com/example/controller/StudentController.java
@@ -1,6 +1,15 @@
 package com.example.controller;
 
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.example.entity.Student;
+
 public class StudentController {
 	//controller//
-
+	
+	@PatchMapping("/student/{id}/{marks}")
+	public Student updateMarks(@PathVariable("id") int id, @PathVariable("marks") int marks) {
+		return null;
+	}
 }
