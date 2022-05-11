e02551dc300bbd896497f1f90a0c20e6d78e7958
SukomalShinde <Sukomalshinde@gmail.comBBBB>
2022-05-10 14:33:23 -1200

    updatedbySukomal

--- src/main/java/com/example/controller/StudentController.java
+++ src/main/java/com/example/controller/StudentController.java
@@ -1,6 +1,23 @@
 package com.example.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.entity.Student;
+import com.example.service.SudentService;
+@RestController
 public class StudentController {
 	//controller//
+	
+	private SudentService service;
+	@GetMapping("/student/{id}")
+	public  Student getStudentById(@PathVariable("id") int id) {
+		
+		return service.getStudentbyid(id);
+	}
+	
+	
 
 }
--- src/main/java/com/example/service/SudentService.java
+++ src/main/java/com/example/service/SudentService.java
@@ -1,5 +1,25 @@
 package com.example.service;
 
-public class SudentService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.example.entity.Student;
+import com.example.repository.StudentRepository;
+
+public class SudentService {
+	
+	@Autowired
+	private StudentRepository repo;
+	
+	
+    public Student getStudentbyid(int id) {
+    Student s1=	repo.findById(id).orElse(null);
+    return s1;
+    }
+
+	
+    
 }
